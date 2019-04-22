43bb6a8e9dcbbd5e8fb1debf2f80e0b13dded188
Nguyen Huy Hung <hungnh103@gmail.com>
2019-04-22 16:31:08 +0700

    Chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/new'
   root 'static_pages#home'
   get 'static_pages/help'
+
+  get "/signup",  to: "users#new"
+  resources :users
 end
